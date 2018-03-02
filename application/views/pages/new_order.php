7e25cc1b8773b7dcb61acb4c86b35f9fd2b742f3
Neil <z5202dbc@gmail.com>
2018-03-02 09:26:16 +0800

    git commit -回複原樣

--- application/views/pages/new_order.php
+++ application/views/pages/new_order.php
@@ -1,233 +1,241 @@
 <body>
     <div>
         <form method="post" name="new_order_info" action="add_order" >
-             
+            <div class="form-group row">
             <label for="example-date-input" class="col-2 col-form-label">日期</label>
-            <input class="form-control" type="date" value="" id="example-date-input">
-
+            <div class="col-10">
+                <input class="form-control" type="date" value="" id="example-date-input">
+            </div>
+            </div>
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">業務</label>
-            <input class="form-control" type="text" value="" id="example-text-input">
-
-            <label for="example-text-input" class="col-2 col-form-label">客戶姓名</label>
-            <input class="form-control" type="text" value="" id="example-text-input">
-             
-            <label for="example-text-input" class="col-2 col-form-label">身分證字號</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
+            <label for="example-text-input" class="col-2 col-form-label">客戶姓名</label>
+            <div class="col-10">
+                <input class="form-control" type="text" value="" id="example-text-input">
+            </div>
+            </div>
+            <div class="form-group row">
+            <label for="example-text-input" class="col-2 col-form-label">身分證字號</label>
+            <div class="col-10">
+                <input class="form-control" type="text" value="" id="example-text-input">
+            </div>
+            </div>
+            <div class="form-group row">
             <label for="example-tel-input" class="col-2 col-form-label">聯絡電話</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="tel" value="" id="example-tel-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">聯絡人</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">聯絡住址</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">買賣</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">股票</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">張數</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">完稅價</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">成交價</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">盤價</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">買賣</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">匯款金額</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">匯款銀行</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">匯款分行</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">匯款戶名</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">轉讓會員</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">完稅人</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">新舊</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">自行應付</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">刻印</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">過戶費</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">媒合</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">過戶日</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">通知查帳</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">契約</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-text-input" class="col-2 col-form-label">稅單</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="text" value="" id="example-text-input">
             </div>
             </div>
-            <!--  
+            <!-- <div class="form-group row">
             <label for="example-search-input" class="col-2 col-form-label">Search</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="search" value="How do I shoot web" id="example-search-input">
             </div>
             </div> -->
-            <!--  
+            <!-- <div class="form-group row">
             <label for="example-email-input" class="col-2 col-form-label">Email</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="email" value="bootstrap@example.com" id="example-email-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-url-input" class="col-2 col-form-label">URL</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="url" value="https://getbootstrap.com" id="example-url-input">
             </div>
             </div> -->
-            <!--  
+            <!-- <div class="form-group row">
             <label for="example-password-input" class="col-2 col-form-label">Password</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="password" value="hunter2" id="example-password-input">
             </div>
             </div> -->
-            <!--  
+            <!-- <div class="form-group row">
             <label for="example-number-input" class="col-2 col-form-label">Number</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="number" value="42" id="example-number-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-datetime-local-input" class="col-2 col-form-label">Date and time</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="datetime-local" value="2011-08-19T13:45:00" id="example-datetime-local-input">
             </div>
             </div> -->
-            <!--  
+            <!-- <div class="form-group row">
             <label for="example-month-input" class="col-2 col-form-label">Month</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="month" value="2011-08" id="example-month-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-week-input" class="col-2 col-form-label">Week</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="week" value="2011-W33" id="example-week-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-time-input" class="col-2 col-form-label">Time</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="time" value="13:45:00" id="example-time-input">
             </div>
             </div>
-             
+            <div class="form-group row">
             <label for="example-color-input" class="col-2 col-form-label">Color</label>
-            
+            <div class="col-10">
                 <input class="form-control" type="color" value="#563d7c" id="example-color-input">
             </div>
             </div> -->
             <button type="submit">送出</button>
-        </form>
-        
+        </form>  
     </div>    
 </body>
