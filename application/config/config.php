47d66fe3eeb45445505456f4c16bb53b92d5371b
DESKTOP-RLL8U2B\datapc <a41104a2003@gmail.com>
2018-03-08 17:24:45 +0800

    將base_url改為138

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/server7/';
+$config['base_url'] = '192.168.147.138/server7/';
 
 /*
 |--------------------------------------------------------------------------
