6bd240faa1a30c9506bd61f7172a49c37e4fd04a
chinklaus <chinklaus@gmail.com>
2018-04-16 18:50:29 +0800

    完成細項編輯modal

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -12,20 +12,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <meta name="description" content="">
         <meta name="author" content="">
         <link rel="icon" href="../../../../favicon.ico">
-
         <title>Server 7</title>
 
         <!-- Bootstrap core CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="<?php echo base_url(); ?>assets/js/jquery-min.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/action.js"></script>
+        <!-- Icons -->
+        <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+        <!-- Graphs -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
         <!-- <link href="../../../../dist/css/bootstrap.min.css" rel="stylesheet"> -->
 
         <!-- Custom styles for this template -->
         <!-- <link href="dashboard.css" rel="stylesheet"> -->
-
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script> -->
         <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
-            
+
     </head>
 
     <style type="text/css">
@@ -36,7 +39,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         #upload {
         width: 100px;
         }
-    
+
         .modal {
         display: none; /* Hidden by default */
         position: fixed; /* Stay in place */
@@ -98,9 +101,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <?php if(isset($_SESSION['NAME'])) echo $_SESSION['NAME'].$_SESSION['LEVEL']; ?>
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
         <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Server 7 後台</a>
-        
+
           <input form="search" class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-        
+
         <ul class="navbar-nav px-3">
             <li class="nav-item text-nowrap">
                 <a class="nav-link" href="<?php echo base_url(); ?>index.php/login/logout">Sign out</a>
@@ -148,7 +151,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                             服務工單
                             </a>
                         </li> -->
-                        <?php 
+                        <?php
                             if ($_SESSION['NAME']=='JOY'&&$_SESSION['LEVEL']=='2') { ?>
                                 <li class="nav-item">
                                     <a class="nav-link" href="<?php echo base_url(); ?>index.php/login/account">
@@ -158,8 +161,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 </li>
                         <?php  }
                         ?>
-                        
-                        
+
+
                     </ul>
                 </div>
             </nav>
