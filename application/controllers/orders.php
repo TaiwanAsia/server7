509b4b10554d030b2840dc7bb19e4dfabefdc9b3
TINO\Tino <tinozxc123@gmail.com>
2018-06-06 17:27:56 +0800

    修改日期格式Y-m-d為Y/m/d，現在不透過匯入會產生1970/1/1預設日期

--- application/controllers/orders.php
+++ application/controllers/orders.php
@@ -671,7 +671,7 @@ class Orders extends CI_Controller {
 
 			$section->addText("TO：".$_POST["dealer_name"], $fontStyle);
 			$section->addText("傳真：".$_POST["dealer_fax"]." "."電話：".$_POST["dealer_tel"], $fontStyle);
-			$section->addText($_POST["transaction_date"]." 成交交易明細", $fontStyle);
+			$section->addText(date("Y/m/d", strtotime($_POST["transaction_date"]))." 成交交易明細", $fontStyle);
 
 			$styleTable = ['borderSize' => 6, 'borderColor' => '999999'];
 			$phpWord->addTableStyle('Colspan Rowspan', $styleTable);
@@ -713,7 +713,7 @@ class Orders extends CI_Controller {
 
 			$row = $table->addRow();
 			$row->addCell()->addText('交割日', $fontStyle);
-			$row->addCell(null, ['gridSpan' => 5])->addText($_POST["transfer_date"], $fontStyle);
+			$row->addCell(null, ['gridSpan' => 5])->addText(date("Y/m/d", strtotime($_POST["transfer_date"])), $fontStyle);
 
 			$row = $table->addRow();
 			$row->addCell()->addText('備註', $fontStyle);
